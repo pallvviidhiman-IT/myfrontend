3d9a11c73e7898f6283c1e40a743ab7d7e00aa33
abc <abc@gmail.com>
2026-05-08 14:35:43 +0530

    fixed app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,57 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [users, setUsers] = useState([]);
+ const [users, setUsers] = useState([]);
+ const [name, setName] = useState("");
+ const [editId, setEditId] = useState(null);
 
-  useEffect(() => {
-    fetch("http://localhost:5000/users")
-      .then((res) => res.json())
-      .then((data) => setUsers(data));
-  }, []);
+ useEffect(() => { getUsers(); }, []);
 
-function addUser() {
+ function getUsers() {
+ fetch("http://localhost:5000/users")
+ .then((res) => res.json())
+ .then((data) => setUsers(data));
+ }
+
+ function addUser() {
  fetch("http://localhost:5000/users", {
  method: "POST",
  headers: { "Content-Type": "application/json" },
  body: JSON.stringify({ name: name })
- })
- .then((res) => res.json())
- .then(() => {
- getUsers(); // Refresh the list
- setName(""); // Clear the input
- });
-}
+ }).then(() => { getUsers(); setName(""); });
+ }
 
-return (
+ function updateUser() {
+ fetch(`http://localhost:5000/users/${editId}`, {
+ method: "PUT",
+ headers: { "Content-Type": "application/json" },
+ body: JSON.stringify({ name: name })
+ }).then(() => { getUsers(); setName(""); setEditId(null); });
+ }
+
+ function deleteUser(id) {
+ fetch(`http://localhost:5000/users/${id}`, { method: "DELETE" })
+ .then(() => getUsers());
+ }
+
+ function handleEdit(user) { setName(user.name); setEditId(user._id); }
+
+ return (
  <div>
- <h1>User App</h1>
- <input
- value={name}
- placeholder="Enter Name"
- onChange={(e) => setName(e.target.value)}
- />
- <button onClick={addUser}>Add User</button>
+ <h1>User Management</h1>
+ <input value={name} placeholder="Enter Name"
+ onChange={(e) => setName(e.target.value)} />
+ {editId
+ ? <button onClick={updateUser}>Update User</button>
+ : <button onClick={addUser}>Add User</button>}
  {users.map((user) => (
- <h2 key={user._id}>{user.name}</h2>
+ <div key={user._id}>
+ <h2>{user.name}</h2>
+ <button onClick={() => handleEdit(user)}>Edit</button>
+ <button onClick={() => deleteUser(user._id)}>Delete</button>
+ </div>
  ))}
  </div>
-);
-
-  return (
-    <div>
-      <h1>Users</h1>
-      {users.map((user) => (
-        <h2 key={user._id}>{user.name}</h2>
-      ))}
-    </div>
-  );
+ );
 }
-
 export default App;
